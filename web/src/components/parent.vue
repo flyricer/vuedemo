6e7f61878e371b41a86e65282264b0ebebb14ff9
flyricer <flyricer@163.com>
2019-06-22 17:14:15 +0800

    updata

---
+++ web/src/components/parent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="parent">
+    <brother-card></brother-card>
+    <sister-card></sister-card>
+  </div>
+</template>
+
+<script>
+import BrotherCard from "@/components/brother.vue";
+import SisterCard from "@/components/sister.vue";
+
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {},
+  computed: {},
+  components: {
+    BrotherCard,
+    SisterCard
+  }
+};
+</script>
+
+<style lang="less" scoped></style>
--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <base-list :user="username"></base-list>
+    <base-list></base-list>
   </div>
 </template>
 
